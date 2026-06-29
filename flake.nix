6a43c32773253b7a93ff21fd56602574e7be51cd
Heitor Toledo Lassarote de Paula <heitor.toledo@serokell.io>
2026-06-29 12:40:04 -0300

    [#2] Set up Nix-based CI with GitHub Actions
    Problem: The repository has no CI, so there is no automated check
    that the code builds, tests pass, or style and licence rules are
    followed.
    
    Solution: Initialise a flake from serokell/nix-templates#haskell-library
    and configure self-hosted nix runners with jobs for build/test matrix,
    hlint, stylish-haskell, hpack sync, REUSE lint, and trailing
    whitespace. Add a weekly cabal-outdated check. Wire up status checks
    in the master branch protection.

---
+++ flake.nix
@@ -0,0 +1,112 @@
+# SPDX-FileCopyrightText: 2026 Serokell <https://serokell.io/>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+{
+  nixConfig = {
+    flake-registry = "https://github.com/serokell/flake-registry/raw/master/flake-registry.json";
+  };
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      flake = false;
+    };
+    haskell-nix = {
+      inputs.hackage.follows = "hackage";
+      inputs.stackage.follows = "stackage";
+    };
+    hackage = {
+      flake = false;
+    };
+    stackage = {
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, haskell-nix, hackage, stackage, serokell-nix, flake-compat, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        haskellPkgs = haskell-nix.legacyPackages."${system}";
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            serokell-nix.overlay
+          ];
+        };
+
+        lib = pkgs.lib;
+        ci = serokell-nix.lib.haskell.makeCI haskellPkgs {
+          # specify the path to the root of your haskell project (the directory containing stack.yaml or cabal.project)
+          src = ./.;
+          # you can specify a list of ghc versions to build packages,
+          # if not specified the ghc versions will be taken from tested-with stanzas from .cabal files
+          # ghcVersions = [ "ghc902" "ghc926" ];
+          # you can specify additional stack yaml files in addition to stack.yaml
+          # stackFiles = [ "stack-lts-21-5.yaml" ];
+          # you can specify additional stack resolvers, they will be replaced in stack.yaml
+          # resolvers = [ "lts-19.13" ];
+          # you can disable building with stack if your project does not use stack
+          buildWithStack = false;
+          # haskell.nix configuration
+          extraArgs = {
+            modules = [
+              (serokell-nix.lib.haskell.optionsLocalPackages {
+                ghcOptions = [
+                  # fail on warnings
+                  "-Werror"
+                  # disable optimisations, we don't need them if we don't package or deploy the executable
+                  "-O0"
+                ];
+              })
+            ];
+          };
+        };
+
+        # Uncomment if your project uses stack2cabal to generate cabal files
+        # stack2cabal = haskellPkgs.haskell.lib.overrideCabal haskellPkgs.haskellPackages.stack2cabal
+        # (drv: { jailbreak = true; broken = false; });
+
+      in {
+        # nixpkgs revision pinned by this flake
+        legacyPackages = pkgs;
+
+        # Expose the build matrix for GitHub Actions
+        inherit (ci) build-matrix;
+
+        packages.branch-lint =
+          (haskellPkgs.cabalProject' {
+            src = ./.;
+            compiler-nix-name = "ghc9102";
+          }).getComponent "branch-lint:exe:branch-lint";
+
+        devShell = {
+          ci = pkgs.mkShell {
+            buildInputs = [
+              # To avoid version mismatches, use `nix develop .#ci -c hpack`
+              pkgs.hpack
+
+              # Uncomment if your project uses scheduled pipeline for `cabal outdated` check
+              # pkgs.cabal-install
+              # pkgs.curl
+              # # GHC is required for `cabal outdated`
+              # pkgs.ghc
+            ];
+          };
+        };
+
+        # derivations that we can run from CI
+        checks = ci.build-all // ci.test-all // {
+
+          trailing-whitespace = pkgs.build.checkTrailingWhitespace ./.;
+          reuse-lint = pkgs.build.reuseLint ./.;
+          # Uncomment in case your project sources contain bash scripts
+          # shellcheck = pkgs.build.shellcheck ./.;
+
+          hlint = pkgs.build.haskell.hlint ./.;
+          stylish-haskell = pkgs.build.haskell.stylish-haskell ./.;
+          cabal-check = pkgs.build.haskell.cabal-check ./.;
+          hpack = pkgs.build.haskell.hpack ./.
+        };
+      });
+}
